f6ce68a73c9cb35c0ffd12550e0eb60c34e0f459
Elifele Fredrik Imanuel Jelifta <elifele2006@gmail.com>
2025-12-05 18:40:27 +0700

    Update RoomFacilityController

--- app/Http/Controllers/Admin/RoomFacilityController.php
+++ app/Http/Controllers/Admin/RoomFacilityController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Models\RoomFacility;
 
 class RoomFacilityController extends Controller
 {
+    public function __construct()
+    {
+    
+        $this->middleware('auth');
+
+        $this->middleware(function ($request, $next) {
+            if (($request->user()->role ?? '') !== 'admin') {
+                abort(403);
+            }
+            return $next($request);
+        });
+    }
+
+    public function index() : View {
+        $facilities = RoomFacility::orderBy('id','asc') -> paginate(15);
+        return view('admin.roomfac.index', compact('facilities'))
+    }
+
     
 }
